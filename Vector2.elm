2696c0bb35dd6e54b4c09f1243c5694b8bda9405
Hassan Hayat <hassan.hayat7@gmail.com>
2014-10-15 19:45:30 +0100

    improve doc formatting

--- Vector2.elm
+++ Vector2.elm
@@ -1,14 +1,14 @@
 module Vector2 where
 {-| Represents a two dimensional vector.
 
-    # Type and Constructors
-    @docs Vector, origin, xUnit, yUnit
+# Type and Constructors
+@docs Vector, origin, xUnit, yUnit
 
-    # Mathematical Operations
-    @docs add, subtract, multiply, divide, dotMultiply, norm, manhattanNorm, length, scaleBy, distance, manhattanDistance
+# Mathematical Operations
+@docs add, subtract, multiply, divide, dotMultiply, norm, manhattanNorm, length, scaleBy, distance, manhattanDistance
 
-    # Useful aliases and operators
-    @docs (<+>), (<->), (<*>), (</>), (<.>), sub, mul, div, dot
+# Useful aliases and operators
+@docs (<+>), (<->), (<*>), (</>), (<.>), sub, mul, div, dot
 -}
 
 {-| The Vector type for 2-d vector operations. Useful for 2d graphics and vector math -}
@@ -29,36 +29,36 @@ yUnit = Vector 0 1
 
 {-| Vector addition.
 
-    (Vector 2 1) <+> (Vector 4 4) == Vector 6 5
+        (Vector 2 1) <+> (Vector 4 4) == Vector 6 5
 -}
 (<+>) : Vector -> Vector -> Vector
 p <+> q = Vector (p.x + q.x) (p.y + q.y)
 
 {-| Vector addition
 
-    add (Vector 2 1) (Vector 4 4) == Vector 6 5
+        add (Vector 2 1) (Vector 4 4) == Vector 6 5
 -}
 add : Vector -> Vector -> Vector
 add = (<+>)
 
 
 {-| Vector subtraction
-
-    (Vector 3 4) <-> (Vector 2 2) == Vector 1 2
+    
+        (Vector 3 4) <-> (Vector 2 2) == Vector 1 2
 -}
 (<->) : Vector -> Vector -> Vector
 p <-> q = Vector (p.x - q.x) (p.y - q.y)
 
 {-| Vector subtraction
 
-    subtract (Vector 3 4) (Vector 2 2) == Vector 1 2
+        subtract (Vector 3 4) (Vector 2 2) == Vector 1 2
 -}
 subtract : Vector -> Vector -> Vector
 subtract = (<->)
 
 {-| Vector subtraction
 
-    sub (Vector 3 4) (Vector 2 2) == Vector 1 2
+        sub (Vector 3 4) (Vector 2 2) == Vector 1 2
 -}
 sub : Vector -> Vector -> Vector
 sub = subtract
@@ -66,7 +66,7 @@ sub = subtract
 {-| Vector multiplication. Element by element multiplication.
     For dot product see "<.>".
 
-    (Vector 3 4) <*> (Vector 2 2) == Vector 6 8
+        (Vector 3 4) <*> (Vector 2 2) == Vector 6 8
 -}
 (<*>) : Vector -> Vector -> Vector
 p <*> q = Vector (p.x * q.x) (p.y * q.y)
@@ -74,7 +74,7 @@ p <*> q = Vector (p.x * q.x) (p.y * q.y)
 {-| Vector multiplication. Element by element multiplication.
     For dot product see "dotMultiply".
 
-    multiply (Vector 3 4) (Vector 2 2) == Vector 6 8
+        multiply (Vector 3 4) (Vector 2 2) == Vector 6 8
 -}
 multiply : Vector -> Vector -> Vector
 multiply = (<*>)
@@ -82,49 +82,49 @@ multiply = (<*>)
 {-| Vector multiplication. Element by element multiplication.
     For dot product see "dot".
 
-    mul (Vector 3 4) (Vector 2 2) == Vector 6 8
+        mul (Vector 3 4) (Vector 2 2) == Vector 6 8
 -}
 mul : Vector -> Vector -> Vector
 mul = multiply
 
 {-| Vector division. Element by element division. 
     
-    (Vector 6 4) </> (Vector 2 2) == Vector 3 2 
+        (Vector 6 4) </> (Vector 2 2) == Vector 3 2 
 -}
 (</>) : Vector -> Vector -> Vector
 p </> q = Vector (p.x / q.x) (p.y / q.y)
 
 {-| Vector division. Element by element division. 
     
-    divide (Vector 6 4) (Vector 2 2) == Vector 3 2 
+        divide (Vector 6 4) (Vector 2 2) == Vector 3 2 
 -}
 divide : Vector -> Vector -> Vector
 divide = (</>)
 
 {-| Vector division. Element by element division. 
     
-    div (Vector 6 4) (Vector 2 2) == Vector 3 2 
+        div (Vector 6 4) (Vector 2 2) == Vector 3 2 
 -}
 div : Vector -> Vector -> Vector
 div = divide
 
 {-| Dot product. 
     
-    (Vector 3 4) <.> (Vector 2 2) == 14
+        (Vector 3 4) <.> (Vector 2 2) == 14
 -}
 (<.>) : Vector -> Vector -> Float
 p <.> q = p.x * q.x + p.y * q.y
 
 {-| Dot product. 
     
-    dotMultiply (Vector 3 4) (Vector 2 2) == 14
+        dotMultiply (Vector 3 4) (Vector 2 2) == 14
 -}
 dotMultiply : Vector -> Vector -> Float
 dotMultiply = (<.>)
 
 {-| Dot product. 
     
-    dot (Vector 3 4) (Vector 2 2) == 14
+        dot (Vector 3 4) (Vector 2 2) == 14
 -}
 dot : Vector -> Vector -> Float
 dot = dotMultiply
@@ -133,7 +133,7 @@ dot = dotMultiply
 {-| Find the nth norm of a vector. This is a generalized form of length or distance.
     Note that typical pythagorean length or euclidean distance is just "norm 2".
 
-    norm 2 (Vector 5 12) == 13
+        norm 2 (Vector 5 12) == 13
 -}
 norm : number -> Vector -> number
 norm n v = ((v.x ^ n) + (v.y ^ n)) ^ (1 / n)
@@ -146,14 +146,14 @@ manhattanNorm v = (abs v.x) + (abs v.y)
     This is the most typical measure for the length of a vector. 
     Use this if unsure. 
 
-    length (Vector 5 12) == 13
+        length (Vector 5 12) == 13
 -}
 length : Vector -> number
 length = norm 2
 
 {-| Scalar multiplication. Scale a vector by some value
     
-    scaleBy 3 (Vector 2 8) == Vector 6 24
+        scaleBy 3 (Vector 2 8) == Vector 6 24
 -}
 scaleBy : number -> Vector -> Vector 
 scaleBy n v = Vector (v.x * n) (v.y * n)
